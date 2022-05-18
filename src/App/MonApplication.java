b3be79163c88ac0964c277f11d7c4db026559cfa
ccrispel <clement.crispel@gmail.com>
2022-05-18 10:05:47 +0200

    ajout source

--- src/App/MonApplication.java
+++ src/App/MonApplication.java
@@ -1,15 +1,21 @@
 package App;
+import io.jbotsim.core.Color;
+import io.jbotsim.core.Node;
 import io.jbotsim.core.Topology;
+import io.jbotsim.core.event.SelectionListener;
 import io.jbotsim.ui.JTopology;
 
 import javax.swing.*;
+import javax.xml.transform.Source;
+
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MonApplication implements ActionListener {
+public class MonApplication implements ActionListener , SelectionListener {
     Topology tp; // Objet qui contient le graphe
     JTopology jtp; // Composant graphique qui affiche le graphe
+    Node source , destination = new Node();
 
     public MonApplication() {
         // Création du graphe
@@ -29,11 +35,18 @@ public class MonApplication implements ActionListener {
         window.add(jtp);
 
         // Création d'un bouton test
-        JButton button = new JButton("Test");
-        window.add(button,BorderLayout.NORTH);
+        JButton topButton = new JButton("Top");
+        window.add(topButton,BorderLayout.NORTH);
+
+        JButton downButton = new JButton("Down");
+        window.add(downButton,BorderLayout.SOUTH);
 
         // Abonnement aux évènements du bouton (clic, etc.)
-        button.addActionListener(this);
+        topButton.addActionListener(this);
+        downButton.addActionListener(this);
+
+        //
+        tp.addSelectionListener(this);
 
         // Finalisation
         window.pack();
@@ -42,8 +55,24 @@ public class MonApplication implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("Test")) {
-            JOptionPane.showMessageDialog(null, "Bouton cliqué");
+        if (e.getActionCommand().equals("Top")) {
+            JOptionPane.showMessageDialog(null, "Bouton haut cliqué");
+        }
+        if (e.getActionCommand().equals("Down")) {
+            JOptionPane.showMessageDialog(null, "Bouton bas cliqué");
+        }
+    }
+
+    @Override
+    public void onSelection(Node selectedNode) {
+        if(destination == null && source!=null ){
+            destination = selectedNode;
+            System.out.println("Destination");
+        }
+        if(source == null){
+            source = selectedNode;
+            source.setColor(Color.black);
+            System.out.println("Source");
         }
     }
 
